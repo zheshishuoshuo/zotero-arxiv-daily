6bdb4ffad01ac4369a818c117bee049a25260685
Shuo <90323232+zheshishuoshuo@users.noreply.github.com>
2025-07-08 12:22:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -162,6 +162,7 @@ if __name__ == '__main__':
         corpus = filter_corpus(corpus, args.zotero_ignore)
         logger.info(f"Remaining {len(corpus)} papers after filtering.")
     logger.info("Retrieving Arxiv papers...")
+    print(args.arxiv_query)
     papers = get_arxiv_paper(args.arxiv_query, args.debug)
     if len(papers) == 0:
         logger.info("No new papers found. Yesterday maybe a holiday and no one submit their work :). If this is not the case, please check the ARXIV_QUERY.")
