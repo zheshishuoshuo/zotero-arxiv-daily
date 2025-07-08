d66e8cb0a7eb072a521d24bb23f43a0e5d02e280
Shuo <90323232+zheshishuoshuo@users.noreply.github.com>
2025-07-08 13:41:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,25 +51,16 @@ def get_arxiv_paper(query:str, debug:bool=False) -> list[ArxivPaper]:
     feed = feedparser.parse(f"https://rss.arxiv.org/atom/{query}")
     if 'Feed error for query' in feed.feed.title:
         raise Exception(f"Invalid ARXIV_QUERY: {query}.")
-    if not debug:
-        papers = []
-        all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries if i.arxiv_announce_type == 'new']
-        bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
-        for i in range(0,len(all_paper_ids),50):
-            search = arxiv.Search(id_list=all_paper_ids[i:i+50])
-            batch = [ArxivPaper(p) for p in client.results(search)]
-            bar.update(len(batch))
-            papers.extend(batch)
-        bar.close()
 
-    else:
-        logger.debug("Retrieve 5 arxiv papers regardless of the date.")
-        search = arxiv.Search(query='cat:cs.AI', sort_by=arxiv.SortCriterion.SubmittedDate)
-        papers = []
-        for i in client.results(search):
-            papers.append(ArxivPaper(i))
-            if len(papers) == 5:
-                break
+    papers = []
+    all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries if i.arxiv_announce_type == 'new']
+    bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
+    for i in range(0,len(all_paper_ids),50):
+        search = arxiv.Search(id_list=all_paper_ids[i:i+50])
+        batch = [ArxivPaper(p) for p in client.results(search)]
+        bar.update(len(batch))
+        papers.extend(batch)
+    bar.close()
 
     return papers
 
